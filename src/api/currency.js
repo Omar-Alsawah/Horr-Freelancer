986a2d053635646882c3c65ade6a247993323b51
Zoon <zoonattack18@gmail.com>
2026-06-21 06:05:33 +0300

    YES

--- src/api/currency.js
+++ src/api/currency.js
@@ -1,6 +1,7 @@
 import axios from './axios';
+import { ENDPOINTS } from './endpoints';
 
 export const currencyApi = {
   convertCurrency: (amount, from, to) => 
-    axios.get(`/currency/convert?amount=${amount}&from=${from}&to=${to}`)
+    axios.get(ENDPOINTS.CURRENCY.CONVERT + `?amount=${amount}&from=${from}&to=${to}`)
 };
